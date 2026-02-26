40b0836e59e03e96d3c3cfa6c1165405b572eb17
Avraneel Pal <avraneelpal22@gmail.com>
2026-02-26 23:53:00 +0530

    Organize Files

--- tests/capitalizer.test.js
+++ tests/capitalizer.test.js
@@ -1,6 +1,6 @@
 // index.test.js
 
-import { capitalize } from "./capitalizer";
+import { capitalize } from "../src/capitalizer";
 
 test("capital test", () => {
   expect(capitalize("paris")).toBe("Paris");
