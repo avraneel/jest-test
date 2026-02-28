7745b14ea2d1d18e825f94930f87a6908f8f1165
Avraneel Pal <avraneelpal22@gmail.com>
2026-02-28 23:10:29 +0530

    Add analyze array

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,5 +1,32 @@
 export function analyzeArray(array) {
   let ob = {};
   ob.length = array.length;
-  console.log(array.reduce((acc, cur) => acc + cur));
+  ob.average = array.reduce((acc, cur) => acc + cur) / ob.length;
+  ob.min = calcMin(array);
+  ob.max = calcMax(array);
+  return ob;
 }
+
+function calcMin(array) {
+  let min = array[0];
+  let minind = 0;
+  for (const el of array) {
+    if (el < min) {
+      min = el;
+      minind = array.indexOf(el);
+    }
+  }
+  return min;
+}
+
+function calcMax(array) {
+  let max = array[0];
+  for (const el of array) {
+    if (el > max) {
+      max = el;
+    }
+  }
+  return max;
+}
+
+analyzeArray([1, 8, 3, 4, 2, 6]);
