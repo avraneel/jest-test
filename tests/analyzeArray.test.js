abd4aa9997e46802cec43857e1d0ee85af19e4a5
Avraneel Pal <avraneelpal22@gmail.com>
2026-02-28 23:11:10 +0530

    Add analyze array test

---
+++ tests/analyzeArray.test.js
@@ -0,0 +1,11 @@
+import { analyzeArray } from "../src/analyzeArray";
+
+test("Analyze Array", () => {
+  const res = {
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  };
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toStrictEqual(res);
+});
---
+++ tests/caesarCipher.test.js
@@ -0,0 +1,7 @@
+import { caesarCipher } from "../src/caesarCipher";
+
+test("Caesar Cipher", () => {
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+  expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
+  expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+});
